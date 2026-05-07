5e629c63ccf1955a85b0c452b140dd2ddd4e0a89
Ram Mohan Reddy Seelam <srammohanreddy23@gmail.com>
2026-05-07 22:21:31 +0530

    leetcode 206

--- Leetcode206.java
+++ Leetcode206.java
@@ -9,7 +9,25 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Leetcode206 {
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
