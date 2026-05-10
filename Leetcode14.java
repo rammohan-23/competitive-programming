468a5ee5f26389dafdededd1d50a6b131dc7e9cf
Ram Mohan Reddy Seelam <53344041+rammohan-23@users.noreply.github.com>
2026-05-10 22:46:54 +0530

    Implement longestCommonPrefix method

---
+++ Leetcode14.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String ans = "";
+        String pref = strs[0];
+        for (int i=1;i<strs.length;i++){
+            while(!strs[i].startsWith(pref)){
+                pref = pref.substring(0,pref.length()-1);
+            }
+        }
+        return pref;
+    }
+}
